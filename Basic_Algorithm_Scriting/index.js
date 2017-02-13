2ef1c8a71a83078ad30050f81e40a212bd10e5a2
Igor Konovalov <konovalov.avp@gmail.com>
2017-02-13 19:08:31 +0300

    largest number in array

--- Basic_Algorithm_Scriting/index.js
+++ Basic_Algorithm_Scriting/index.js
@@ -1,12 +1,12 @@
 // Return the length of the longest word in the provided sentence.
-// function findLongestWord(str) {
-//   let sorted = str.split(' ').sort((prev, next) => {
-//     return next.length - prev.length;
-//   });
-//   return sorted[0].length;
-// }
-//
-//
+function findLongestWord(str) {
+  let sorted = str.split(' ').sort((prev, next) => {
+    return next.length - prev.length;
+  });
+  return sorted[0].length;
+}
+
+
 // console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 
 //Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
@@ -22,5 +22,19 @@ function titleCase(str) {
   return newWords.join(' ');
 }
 
-console.log(titleCase("I'm a little tea pot"));
-console.log('foo');
+// titleCase("I'm a little tea pot");
+
+// Return an array consisting of the largest number from each provided sub-array.
+
+function largestOfFour(arr) {
+  let largest = [];
+  arr.forEach((subArr) => {
+    subArr.sort((a,b) => {
+      return b - a;
+    });
+    largest.push(subArr[0]);
+  })
+  return largest;
+}
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
