e201f79b62e7434b98b71eb291dfeb98639cb2f6
Igor Konovalov <konovalov.avp@gmail.com>
2017-02-13 17:41:41 +0300

    first letter uppercase

--- Basic_Algorithm_Scriting/build/index.es5.js
+++ Basic_Algorithm_Scriting/build/index.es5.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Return the length of the longest word in the provided sentence.
 // function findLongestWord(str) {
@@ -14,7 +14,37 @@
 //Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
 
 function titleCase(str) {
-  return str;
+  var strArray = str.toLowerCase().split(' ');
+  var newWords = [];
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = strArray[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var word = _step.value;
+
+      var wordArray = word.split('');
+      wordArray[0] = wordArray[0].toUpperCase();
+      newWords.push(wordArray.join(''));
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return newWords.join(' ');
 }
 
-titleCase("I'm a little tea pot");
+console.log(titleCase("I'm a little tea pot"));
+console.log('foo');
--- Basic_Algorithm_Scriting/index.js
+++ Basic_Algorithm_Scriting/index.js
@@ -12,7 +12,15 @@
 //Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
 
 function titleCase(str) {
-  return str;
+  let strArray = str.toLowerCase().split(' ');
+  let newWords = [];
+  for (var word of strArray) {
+    let wordArray = word.split('');
+    wordArray[0] = wordArray[0].toUpperCase();
+    newWords.push(wordArray.join(''));
+  }
+  return newWords.join(' ');
 }
 
-titleCase("I'm a little tea pot");
+console.log(titleCase("I'm a little tea pot"));
+console.log('foo');
