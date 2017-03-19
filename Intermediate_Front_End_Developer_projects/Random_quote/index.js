c96d9d8e466505dbdc6663a731f9cd5b53270039
Igor Konovalov <konovalov.avp@gmail.com>
2017-03-19 17:07:57 +0300

    random quote done

--- Intermediate_Front_End_Developer_projects/Random_quote/index.js
+++ Intermediate_Front_End_Developer_projects/Random_quote/index.js
@@ -1,4 +1,3 @@
-
 const myHeaders = new Headers({
   'X-Mashape-Key': "OYxVA1zIhrmsh4ClMqZgdprhVGZMp1AsZBwjsn5xiAE5adD0Vu",
   "Content-Type": "application/x-www-form-urlencoded",
@@ -10,7 +9,38 @@ const myInit = { method: 'GET',
                mode: 'cors',
                cache: 'default' }
 
+const TWITTER_URL = 'https://twitter.com/intent/tweet?text='
+const TWITTER_HASHTAG = 'button_hashtag='
 
-fetch('https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous', myInit)
-  .then(response => response.json())
-  .then(data => console.log(data))
+const quote = document.getElementById('quote')
+const author = document.getElementById('author')
+const tweet = document.getElementById('tweet')
+const container = document.getElementsByTagName('body')[0]
+const loadQuoteButton = document.getElementById('more')
+
+randomInteger = (min, max) => {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+getQuote = () => {
+  fetch('https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous', myInit)
+    .then(response => response.json())
+    .then(data => {
+      quote.innerHTML = data.quote
+      author.innerHTML = `${data.author}`
+      tweet.setAttribute(
+        'href',
+        `${TWITTER_URL}${encodeURIComponent(data.quote)}&${TWITTER_HASHTAG}${data.author}`
+      )
+      const color = randomInteger(0, 255)
+      container.setAttribute('style', `background-color: hsl(${color}, 50%, 50%)`)
+      tweet.setAttribute('style', `background-color: hsl(${color}, 50%, 50%)`)
+      loadQuoteButton.setAttribute('style', `background-color: hsl(${color}, 50%, 50%)`)
+    })
+}
+
+loadQuoteButton.addEventListener('click', () => {
+  getQuote()
+})
+
+getQuote()
