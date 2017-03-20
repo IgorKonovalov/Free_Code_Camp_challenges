ae91c4dedd25a1e3b97798f9e0be20e36ede8bfd
IgorKonovalov <konovalov.avp@gmail.com>
2017-03-20 21:55:08 +0300

    weather app finished

--- Intermediate_Front_End_Developer_projects/Local_Weather/index.js
+++ Intermediate_Front_End_Developer_projects/Local_Weather/index.js
@@ -22,6 +22,10 @@ const countryText = document.getElementById('country')
 const cityText = document.getElementById('city')
 const weatherIcon = document.getElementById('weatherIcon')
 const temperatureText = document.getElementById('temperature')
+const body = document.getElementsByTagName('body')[0]
+
+let celsius = true
+let temperature = 0
 
 navigator.geolocation.getCurrentPosition((position) => {
   fetch(
@@ -37,6 +41,38 @@ weatherResolver = data => {
   countryText.innerHTML = data.query.results.channel.location.country
   cityText.innerHTML = data.query.results.channel.location.city
   const iconCode = data.query.results.channel.item.condition.code
-  weatherIcon.setAttribute('class', `wi wi-${ICONS[iconCode]}`)
-  temperatureText.innerHTML = `${data.query.results.channel.item.condition.temp}℃`
+  if (iconCode == 3200) {
+    weatherIcon.setAttribute('class', `wi wi-${ICONS[48]}`)
+  } else {
+    weatherIcon.setAttribute('class', `wi wi-${ICONS[iconCode]}`)
+  }
+  temperature = data.query.results.channel.item.condition.temp
+  temperatureText.innerHTML = `${temperature}℃`
 }
+
+temperatureText.addEventListener('click', () => {
+  if (celsius) {
+    temperatureText.innerHTML = `${temperature * 1.8 + 32}°F`
+    celsius = false
+  } else {
+    temperatureText.innerHTML = `${temperature}℃`
+    celsius = true
+  }
+})
+
+const now = new Date
+const hours = now.getHours()
+
+colorBackground = (hours) => {
+  if (hours <= 7) {
+    body.setAttribute('style', 'background: linear-gradient(to left, #141E30 , #243B55)')
+  } else if ((hours <= 12) && (hours > 7)) {
+    body.setAttribute('style', 'background: linear-gradient(to left, #FF5F6D , #FFC371)')
+  } else if ((hours <= 20) && (hours > 12)) {
+    body.setAttribute('style', 'background: linear-gradient(to left, #649173 , #DBD5A4)')
+  } else if ((hours <= 21) && (hours > 20)) {
+    body.setAttribute('style', 'background: linear-gradient(to left, #000000 , #53346D)')
+  }
+}
+
+colorBackground(hours)
